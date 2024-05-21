351b622523bee8fa23b2bed355eae49b676e3cde
mariusz komputer adama <kontakt@kylo.com.pl>
2024-05-21 16:34:49 +0200

    This commit adds texttest_fixture.go file from original reposity

---
+++ texttest_fixture.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	rose "example/hello/gilded-rose"
+)
+
+func main() {
+	var items = []*rose.Item{
+		{"+5 Dexterity Vest", 10, 20},
+		{"Aged Brie", 2, 0},
+		{"Elixir of the Mongoose", 5, 7},
+		{"Sulfuras, Hand of Ragnaros", 0, 80},
+		{"Sulfuras, Hand of Ragnaros", -1, 80},
+		{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
+		{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
+		{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+		{"Conjured Mana Cake", 3, 6},
+	}
+
+	days := 2
+	var err error
+	if len(os.Args) > 1 {
+		days, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		days++
+	}
+
+	for day := 0; day < days; day++ {
+		fmt.Printf("-------- day %d --------\n", day)
+		fmt.Println("Name, SellIn, Quality")
+		for i := 0; i < len(items); i++ {
+			fmt.Println(items[i])
+		}
+		fmt.Println("")
+		rose.UpdateQuality(items...)
+	}
+}
